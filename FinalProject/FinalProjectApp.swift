faa7f647edd6fe11887e37136bdcbdbb2938f3d4
ImportArcade <colbybarrett99@gmail.com>
2024-07-23 21:24:13 -0600

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Colby Barrett on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
